1fda93613d054ae8732e24e6a99243a89a5b5dda
Dollypee48 <akinolad22@gmail.com>
2025-04-05 09:19:38 +0100

    to-do list

---
+++ script.js
@@ -0,0 +1,126 @@
+
+const taskInput = document.getElementById("task-input");
+const addTask = document.getElementById("add-task");
+const exportTask = document.getElementById("export-task");
+const taskList = document.getElementById("task-list");
+const progressBar = document.getElementById("progress");
+const number = document.getElementById("numbers");
+
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+addTask.addEventListener("click", function() {
+    let taskText = taskInput.value.trim(); 
+    if (taskText !== "") {
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+   
+        let newTask = {
+            text: taskText, 
+            timeStamp: new Date().toISOString(), 
+        };
+
+        tasks.push(newTask);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        displayTask();
+        taskInput.value = "";
+        updateStats()
+    }else {
+        alert("Task cannot be empty")
+    }
+});
+
+function loadTasks() {
+    displayTask();
+}
+
+function displayTask() {
+    taskList.innerHTML = "";
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(task => {
+        if (task.completed === undefined) {
+            task.completed = false;  
+        }
+    });
+
+    tasks.sort((a, b) => new Date(b.timeStamp) - new Date(a.timeStamp));
+
+    tasks.forEach(addTaskToList);
+}
+
+function addTaskToList(task) {
+    let li = document.createElement("li");
+
+    if (task.completed) {
+        li.style.textDecoration = "line-through";
+        li.style.color = "green";
+       
+    }
+
+    li.innerHTML = `
+        <strong> ${task.text}</strong> <br> 
+        <small> ${formatDate(task.timeStamp)}</small>
+    `;
+
+    let completeTaskButton = document.createElement("button");
+    completeTaskButton.innerHTML = "✔️";
+    completeTaskButton.addEventListener("click", function() {
+        completeTask(task.text);
+    });
+
+    let deleteTaskButton = document.createElement("button");
+    deleteTaskButton.innerHTML = "❌";
+    deleteTaskButton.addEventListener("click", function() {
+        deleteTask(task.text); 
+    });
+
+    li.appendChild(completeTaskButton);
+    li.appendChild(deleteTaskButton);
+    taskList.appendChild(li);
+}
+
+function deleteTask(taskText) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks = tasks.filter((task) => task.text !== taskText);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTask();
+    updateStats ()
+}
+
+function completeTask(taskText) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks = tasks.map((task) => {
+        if (task.text === taskText) {
+            task.completed = true;  
+        }
+        return task;
+        
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTask();
+    updateStats();
+}
+
+function formatDate(timeStamp) {
+    let date = new Date(timeStamp);
+    return date.toLocaleString();
+}
+
+function updateStats (){
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    const completedTask = tasks.filter((task) => task.completed).length;
+
+    const totalTasks = tasks.length;
+
+    const progress =(completedTask / totalTasks) * 100;
+
+    progressBar.style.width = `${progress}%`;
+
+    number.innerText = `${completedTask} / ${totalTasks}`;
+
+}
+
+
